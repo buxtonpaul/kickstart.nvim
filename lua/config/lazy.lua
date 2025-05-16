68f81a0dfe98b4394f58c14851f55dc2356a3be2
Paul Buxton <paulbuxton.mail@googlemail.com>
2025-05-16 16:21:01 +0100

    Update lazy settings to be less vebose

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -30,7 +30,8 @@ require('lazy').setup {
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = { enabled = false },
+  change_detection = { enabled = true, notify = false },
 
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
