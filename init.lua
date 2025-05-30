903c12f622befc977b0f92adfffd975509165061
Paul Buxton <paulbuxton.mail@googlemail.com>
2025-05-30 11:15:39 +0100

    Add grugfar and sticky plugins. Make resession use keybindings instead of auto save/load

--- init.lua
+++ init.lua
@@ -51,19 +51,14 @@ vim.cmd.colorscheme 'vscode'
 --start ressession
 local resession = require 'resession'
 require('resession').setup {
-  autosave = {
-    enabled = true,
-    interval = 60,
-    notify = true,
-  },
   extensions = {
     overseer = {},
   },
 }
 -- Resession does NOTHING automagically, so we have to set up some keymaps
--- vim.keymap.set("n", "<leader>ss", resession.save)
--- vim.keymap.set("n", "<leader>sl", resession.load)
--- vim.keymap.set("n", "<leader>sd", resession.delete)
+vim.keymap.set('n', '<leader>rs', resession.save)
+vim.keymap.set('n', '<leader>rl', resession.load)
+vim.keymap.set('n', '<leader>rd', resession.delete)
 
 -- Setup Lualine
 require('lualine').setup {
@@ -109,27 +104,27 @@ require('lualine').setup {
 }
 
 --Setup autocommands, possibly this should be moved to another file and get included....
-vim.api.nvim_create_autocmd('VimEnter', {
-  callback = function()
-    -- Only load the session if nvim was started with no args and without reading from stdin
-    if vim.fn.argc(-1) == 0 and not vim.g.using_stdin then
-      -- Save these to a different directory, so our manual sessions don't get polluted
-      resession.load(vim.fn.getcwd(), { dir = 'dirsession', silence_errors = true })
-    end
-  end,
-  nested = true,
-})
-vim.api.nvim_create_autocmd('VimLeavePre', {
-  callback = function()
-    resession.save(vim.fn.getcwd(), { dir = 'dirsession', notify = false })
-  end,
-})
-vim.api.nvim_create_autocmd('StdinReadPre', {
-  callback = function()
-    -- Store this for later
-    vim.g.using_stdin = true
-  end,
-})
+-- vim.api.nvim_create_autocmd('VimEnter', {
+--   callback = function()
+--     -- Only load the session if nvim was started with no args and without reading from stdin
+--     if vim.fn.argc(-1) == 0 and not vim.g.using_stdin then
+--       -- Save these to a different directory, so our manual sessions don't get polluted
+--       resession.load(vim.fn.getcwd(), { dir = 'dirsession', silence_errors = true })
+--     end
+--   end,
+--   nested = true,
+-- })
+-- vim.api.nvim_create_autocmd('VimLeavePre', {
+--   callback = function()
+--     resession.save(vim.fn.getcwd(), { dir = 'dirsession', notify = false })
+--   end,
+-- })
+-- vim.api.nvim_create_autocmd('StdinReadPre', {
+--   callback = function()
+--     -- Store this for later
+--     vim.g.using_stdin = true
+--   end,
+-- })
 
 require('toggleterm').setup {
   direction = 'float',
@@ -155,6 +150,9 @@ require('quicker').setup {
     },
   },
 }
+
+require('stickybuf').setup()
+
 local harpoon = require 'harpoon'
 
 -- REQUIRED
